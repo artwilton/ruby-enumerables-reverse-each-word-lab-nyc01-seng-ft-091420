e53970c54b0ed4ba0be2d0e0574e3d8ad55b4973
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-11 02:40:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ def reverse_each_word(sentence1)
     array = sentence1.split(" ").each do |word|
        word.reverse!
     end
-    array.join(" ")
-    array
+    array = array.join(" ")
 end
 
 def reverse_each_word(sentence2)
